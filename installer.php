914642f6fa29705d788f58167826ff0ce4580ccc
amadiify <helloamadiify@gmail.com>
2020-09-09 22:48:06 +0100

    default

--- installer.php
+++ installer.php
@@ -419,8 +419,8 @@ endforeach;
 
 // download other required templates
 $requiredTemplates = [
-	'https://raw.githubusercontent.com/wekiwork/moorexa-installer/master/.global.config.frontend' => '.global.config.frontend',
-	'https://raw.githubusercontent.com/wekiwork/moorexa-installer/master/.global.config.service' => '.global.config.service',
+	'https://raw.githubusercontent.com/wekiwork/moorexa-installer/master/.global.config.frontend' => '.global.config.frontend.txt',
+	'https://raw.githubusercontent.com/wekiwork/moorexa-installer/master/.global.config.service' => '.global.config.service.txt',
 	'https://raw.githubusercontent.com/wekiwork/moorexa-installer/master/installer_directory' => 'installer_directory'
 ];	
 
